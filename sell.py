c2247391d619f6c2851eb3b2f71cb94c4afa1b5c
iticopasivagangai-dot <iti.copa.sivagangai@gmail.com>
2026-08-18 16:35:24 +0530

    Update sell.py

--- sell.py
+++ sell.py
@@ -52,7 +52,7 @@ cars = [
         "transmission": "Automatic",
         "city": "Chennai",
         "owner": "First Owner",
-        "icon": "🚙"
+        "image": "https://images.unsplash.com/photo-1606664515524-ed2f786a0bd6"
     },
     {
         "name": "Maruti Swift",
